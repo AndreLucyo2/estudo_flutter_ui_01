20fdb9221e88996b15c6cb9829c124135092e43d
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-24 23:56:37 -0300

    Video #8

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,37 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(AppWidget(titulo: 'video #6'));
+  runApp(AppWidget());
 }
 
-//Video #7 https://youtu.be/nCYtIyrSRpg?list=PLlBnICoI-g-d-J57QIz6Tx5xtUDGQdBFB
+//----------------------------------------------------------------------
 class AppWidget extends StatelessWidget {
-  final String titulo;
-
-  //Construtor recebe o parametro:
-  const AppWidget({Key key, this.titulo}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Container(
-        child: Center(
-          child: Text(titulo),
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: HomePage(),
     );
+  }
+}
+
+//----------------------------------------------------------------------
+//Video #8
+//Home page: pagina com estado
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState('video #6');
+  }
+}
+
+//----------------------------------------------------------------------
+//estado da pagina Home
+class HomePageState extends State<HomePage> {
+  final String titulo;
+  HomePageState(this.titulo);
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text(titulo)));
   }
 }
