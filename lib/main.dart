29eccc409caa253e2fc2881e69d493a8dd4228a1
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-24 23:37:05 -0300

    Feito video #6 https://youtu.be/4CCW_cdVBQg?list=PLlBnICoI-g-d-J57QIz6Tx5xtUDGQdBFB

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(AppWidget());
+  runApp(AppWidget(titulo: 'video #6'));
 }
 
 //Video #6 https://youtu.be/4CCW_cdVBQg
+//widget sem estado, sem animação...
 class AppWidget extends StatelessWidget {
+  final String titulo;
+
+  //Construtor recebe o parametro:
+  const AppWidget({Key key, this.titulo}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    Container(
+    return Container(
       child: Center(
         child: Text(
-          'André teste',
+          titulo,
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 45.0),
         ),
