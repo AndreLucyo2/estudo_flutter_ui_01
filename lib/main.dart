93b6aff7b13e731e8749f71080ec70deaac3b95e
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-25 00:10:23 -0300

    video #8 https://youtu.be/8-mnyze0gKw?list=PLlBnICoI-g-d-J57QIz6Tx5xtUDGQdBFB

--- lib/main.dart
+++ lib/main.dart
@@ -17,22 +17,36 @@ class AppWidget extends StatelessWidget {
 }
 
 //----------------------------------------------------------------------
-//Video #8
+//Video #8 https://youtu.be/8-mnyze0gKw?list=PLlBnICoI-g-d-J57QIz6Tx5xtUDGQdBFB
 //Home page: pagina com estado
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() {
-    return HomePageState('video #6');
+    return HomePageState();
   }
 }
 
 //----------------------------------------------------------------------
 //estado da pagina Home
 class HomePageState extends State<HomePage> {
-  final String titulo;
-  HomePageState(this.titulo);
+  int contador = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text(titulo)));
+    return Container(
+        child: Center(
+      //detecta gestos em um widget toque por exemplo
+      child: GestureDetector(
+        child: Text('tocou :$contador'),
+        //evento de toque: executa quando for clicado:
+        onTap: () {
+          //Modifica o estado da variavel: o setState refaz o Build
+          setState(() {
+            contador++;
+          });
+          print('Clicado :$contador!!!');
+        },
+      ),
+    ));
   }
 }
