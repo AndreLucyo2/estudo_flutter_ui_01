2019a06191a30c2b460fb03bf7d95c1792f06c13
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-24 23:27:11 -0300

    criado classe AppWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Texto na tela do cellular',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 45.0),
+  runApp(AppWidget());
+}
+
+//Video #6 https://youtu.be/4CCW_cdVBQg
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Container(
+      child: Center(
+        child: Text(
+          'André teste',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 45.0),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
