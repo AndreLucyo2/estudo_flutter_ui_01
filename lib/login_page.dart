963b4f4d268b9d2020023e81b839d6fbeeda0efd
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-27 00:15:02 -0300

    Video #17 Alterado para Scaffold , corrido erro do teclado ocultar o botão, cria um scroll usando o SingleChildScrollView()

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,68 +12,72 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          //adicionado Padding
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //Campo de texto:
-              TextField(
-                //Definir o tipo de teclado
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          //pegar as dimeções ta tela:
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+
+          child: Padding(
+            //adicionado Padding
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Campo de texto:
+                TextField(
+                  //Definir o tipo de teclado
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  //Pega o valor do campo:
+                  onChanged: (text) {
+                    email = text;
+                  },
                 ),
-                //Pega o valor do campo:
-                onChanged: (text) {
-                  email = text;
-                },
-              ),
 
-              //Separador
-              Container(height: 20),
+                //Separador
+                Container(height: 20),
 
-              TextField(
-                //Ocultar senha:
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
+                TextField(
+                  //Ocultar senha:
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                  //Pega o valor do campo:
+                  onChanged: (text) {
+                    senha = text;
+                  },
                 ),
-                //Pega o valor do campo:
-                onChanged: (text) {
-                  senha = text;
-                },
-              ),
 
-              //Separador
-              Container(height: 20),
+                //Separador
+                Container(height: 20),
 
-              //Criar um botão
-              ElevatedButton(
-                child: Text('Entrar'),
-                onPressed: () {
-                  //Faz a validação do login:
-                  if (email.isEmpty && senha.isEmpty) {
-                    print('Campo senha e email nao podem ser vazios!');
-                    return;
-                  }
-                  if (email == 'als@gmail.com' && senha == '123') {
-                    print('Parabens voce logou!');
-                    return;
-                  } else {
-                    print('Senha ou email inválidos!');
-                    return;
-                  }
-                },
-              ),
-            ],
+                //Criar um botão
+                ElevatedButton(
+                  child: Text('Entrar'),
+                  onPressed: () {
+                    //Faz a validação do login:
+                    if (email.isEmpty && senha.isEmpty) {
+                      print('Campo senha e email nao podem ser vazios!');
+                      return;
+                    }
+                    if (email == 'als@gmail.com' && senha == '123') {
+                      print('Parabens voce logou!');
+                      return;
+                    } else {
+                      print('Senha ou email inválidos!');
+                      return;
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
