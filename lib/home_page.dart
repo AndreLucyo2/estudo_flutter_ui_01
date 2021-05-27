ea403ec8254c22c83f80ffc41f93c4e0e9831bab
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-26 23:01:43 -0300

    Video #14 https://youtu.be/gA_ZSsl8WrA?list=PLlBnICoI-g-d-J57QIz6Tx5xtUDGQdBFB

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -34,7 +34,7 @@ class HomePageState extends State<HomePage> {
         height: double.infinity,
         //fali a linhamentos da Coluna:
         child: Column(
-          //Alinhamento do eixo principal
+          //Alinhamento do eixo principal Vertical
           mainAxisAlignment: MainAxisAlignment.center,
           //Alinhamento dos filhos, cada um dos filhos
           crossAxisAlignment: CrossAxisAlignment.start,
