749da0d7c732593afbbb0290a3df1ff5edce9267
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-29 22:44:10 -0300

    Video #23

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,91 +10,100 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          //pegar as dimeções ta tela:
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
+  //Video #23 separado Body do Scaffold em um metodo
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        //pegar as dimeções ta tela:
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
 
-          child: Padding(
-            //adicionado Padding
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                //Separador
-                Container(height: 40),
+        child: Padding(
+          //adicionado Padding
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //Separador
+              Container(height: 40),
 
-                //Video #21 Inserindo imagens da internet
-                Container(
-                  width: 100,
-                  height: 100,
-                  //Video #22 - pegando a imagem de forma local
-                  child: Image.asset('assets/images/logo.jpg'),
+              //Video #21 Inserindo imagens da internet
+              Container(
+                width: 100,
+                height: 100,
+                //Video #22 - pegando a imagem de forma local
+                child: Image.asset('assets/images/logo.jpg'),
+              ),
+
+              //Separador
+              Container(height: 30),
+              //Campo de texto:
+              TextField(
+                //Definir o tipo de teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
                 ),
+                //Pega o valor do campo:
+                onChanged: (text) {
+                  email = text;
+                },
+              ),
+              //Separador
+              Container(height: 20),
 
-                //Separador
-                Container(height: 30),
-                //Campo de texto:
-                TextField(
-                  //Definir o tipo de teclado
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                  //Pega o valor do campo:
-                  onChanged: (text) {
-                    email = text;
-                  },
+              TextField(
+                //Ocultar senha:
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
                 ),
-                //Separador
-                Container(height: 20),
+                //Pega o valor do campo:
+                onChanged: (text) {
+                  senha = text;
+                },
+              ),
 
-                TextField(
-                  //Ocultar senha:
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
-                  ),
-                  //Pega o valor do campo:
-                  onChanged: (text) {
-                    senha = text;
-                  },
-                ),
+              //Separador
+              Container(height: 20),
 
-                //Separador
-                Container(height: 20),
-
-                //Criar um botão
-                ElevatedButton(
-                  child: Text('Entrar'),
-                  onPressed: () {
-                    //Faz a validação do login:
-                    if (email.isEmpty && senha.isEmpty) {
-                      print('Campo senha e email nao podem ser vazios!');
-                      return;
-                    }
-                    //Sucesso!
-                    if (email == 'als@gmail.com' && senha == '123') {
-                      print('Parabens voce logou!');
-                      //Video #19
-                      Navigator.of(context).pushNamed('/home');
-                    } else {
-                      print('Senha ou email inválidos!');
-                      return;
-                    }
-                  },
-                ),
-              ],
-            ),
+              //Criar um botão
+              ElevatedButton(
+                child: Text('Entrar'),
+                onPressed: () {
+                  //Faz a validação do login:
+                  if (email.isEmpty && senha.isEmpty) {
+                    print('Campo senha e email nao podem ser vazios!');
+                    return;
+                  }
+                  //Sucesso!
+                  if (email == 'als@gmail.com' && senha == '123') {
+                    print('Parabens voce logou!');
+                    //Video #19
+                    Navigator.of(context).pushNamed('/home');
+                  } else {
+                    print('Senha ou email inválidos!');
+                    return;
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Video #23 Uso do Stack - sbrepoe widget
+      body: Stack(children: [
+        Container(color: Colors.purple),
+        _body(),
+      ]),
     );
   }
 }
